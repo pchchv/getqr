ced5589f093b9f61d4d09d74631788e5a3a0b837
pchchv <ipchchv@gmail.com>
2022-08-25 10:57:03 +0300

    Implement method New of bitset type

--- Bitset/bitset.go
+++ Bitset/bitset.go
@@ -1,7 +1,12 @@
-package getqr
+package bitset
 
 // Bitset stores an array of bits.
-type bitset struct {
+type Bitset struct {
 	numBits int    // The number of bits stored
 	bits    []byte // Storage for individual bits
+}
+
+func New(v ...bool) *Bitset {
+	b := &Bitset{numBits: 0, bits: make([]byte, 0)}
+	return b
 }
