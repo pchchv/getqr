74635d8087c7b7b35e667c3b6c688d833495995b
pchchv <ipchchv@gmail.com>
2022-08-25 10:59:25 +0300

    Implement a ensure capasity method

--- Bitset/bitset.go
+++ Bitset/bitset.go
@@ -10,3 +10,20 @@ func New(v ...bool) *Bitset {
 	b := &Bitset{numBits: 0, bits: make([]byte, 0)}
 	return b
 }
+
+// Ensures the Bitset can store an additional numBits. The underlying array is expanded if necessary.
+// To prevent frequent reallocation, expanding the underlying array at least doubles its capacity.
+func (b *Bitset) ensureCapacity(numBits int) {
+	numBits += b.numBits
+
+	newNumBytes := numBits / 8
+	if numBits%8 != 0 {
+		newNumBytes++
+	}
+
+	if len(b.bits) >= newNumBytes {
+		return
+	}
+
+	b.bits = append(b.bits, make([]byte, newNumBytes+2*len(b.bits))...)
+}
