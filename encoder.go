981b8e3b281626f52f20347a123039509e548a28
pchchv <ipchchv@gmail.com>
2022-08-25 11:16:10 +0300

    Add function that creates new data encoder

--- encoder.go
+++ encoder.go
@@ -1,10 +1,24 @@
 package getqr
 
+import (
+	"log"
+
+	bitset "github.com/pchchv/getqr/bitset"
+)
+
+const (
+	dataEncoderType1To9 dataEncoderType = iota
+	dataEncoderType10To26
+	dataEncoderType27To40
+)
+
 // A dataEncoder encodes data for a particular QR Code version
 type dataEncoder struct {
-	numericModeIndicator      *bitset // Mode indicator bit sequences.
-	alphanumericModeIndicator *bitset
-	byteModeIndicator         *bitset
+	minVersion                int            //Minimum version supported
+	maxVersion                int            // Maximum version supported.
+	numericModeIndicator      *bitset.Bitset // Mode indicator bit sequences.
+	alphanumericModeIndicator *bitset.Bitset
+	byteModeIndicator         *bitset.Bitset
 	numericCharCountBits      int // Character count lengths
 	alphanumericCharCountBits int
 	byteCharCountBits         int
@@ -13,6 +27,8 @@ type dataEncoder struct {
 	optimised                 []segment // The data classified into optimised segments.
 }
 
+type dataEncoderType uint8
+
 // A segment encoding mode
 type dataMode uint8
 
@@ -20,4 +36,46 @@ type dataMode uint8
 type segment struct {
 	dataMode dataMode // Data Mode (e.g. numeric)
 	data     []byte   // segment data (e.g. "abc")
+}
+
+func newDataEncoder(t dataEncoderType) *dataEncoder {
+	d := &dataEncoder{}
+	switch t {
+	case dataEncoderType1To9:
+		d = &dataEncoder{
+			minVersion:                1,
+			maxVersion:                9,
+			numericModeIndicator:      bitset.New(false, false, false, true),
+			alphanumericModeIndicator: bitset.New(false, false, true, false),
+			byteModeIndicator:         bitset.New(false, true, false, false),
+			numericCharCountBits:      10,
+			alphanumericCharCountBits: 9,
+			byteCharCountBits:         8,
+		}
+	case dataEncoderType10To26:
+		d = &dataEncoder{
+			minVersion:                10,
+			maxVersion:                26,
+			numericModeIndicator:      bitset.New(false, false, false, true),
+			alphanumericModeIndicator: bitset.New(false, false, true, false),
+			byteModeIndicator:         bitset.New(false, true, false, false),
+			numericCharCountBits:      12,
+			alphanumericCharCountBits: 11,
+			byteCharCountBits:         16,
+		}
+	case dataEncoderType27To40:
+		d = &dataEncoder{
+			minVersion:                27,
+			maxVersion:                40,
+			numericModeIndicator:      bitset.New(false, false, false, true),
+			alphanumericModeIndicator: bitset.New(false, false, true, false),
+			byteModeIndicator:         bitset.New(false, true, false, false),
+			numericCharCountBits:      14,
+			alphanumericCharCountBits: 13,
+			byteCharCountBits:         16,
+		}
+	default:
+		log.Panic("Unknown dataEncoderType")
+	}
+	return d
 }
