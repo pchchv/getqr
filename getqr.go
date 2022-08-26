f5e23e45670ee0ccf80c38f02ca0dfffd02c1cd9
pchchv <ipchchv@gmail.com>
2022-08-26 10:02:34 +0300

    Implement a New function creates new QR code

--- getqr.go
+++ getqr.go
@@ -1,6 +1,7 @@
 package getqr
 
 import (
+	"errors"
 	"image/color"
 
 	bitset "github.com/pchchv/getqr/bitset"
@@ -18,4 +19,40 @@ type QRCode struct {
 	data            *bitset.Bitset
 	synbol          *symbol
 	mask            int
+}
+
+// Constructs a QR Code. An error occurs if the content is too long
+func New(content string, level RecoveryLevel) (*QRCode, error) {
+	var err error
+	var encoder *dataEncoder
+	var encoded *bitset.Bitset
+	var chosenVersion *qrCodeVersion
+	encoders := []dataEncoderType{dataEncoderType1To9, dataEncoderType10To26, dataEncoderType27To40}
+	for _, t := range encoders {
+		encoder = newDataEncoder(t)
+		encoded, err = encoder.encode([]byte(content))
+		if err != nil {
+			continue
+		}
+		chosenVersion = chooseQRCodeVersion(level, encoder, encoded.Len())
+		if chosenVersion != nil {
+			break
+		}
+	}
+	if err != nil {
+		return nil, err
+	} else if chosenVersion == nil {
+		return nil, errors.New("content too long to encode")
+	}
+	q := &QRCode{
+		Content:         content,
+		Level:           level,
+		VersionNumber:   chosenVersion.version,
+		ForegroundColor: color.Black,
+		BackgroundColor: color.White,
+		encoder:         encoder,
+		data:            encoded,
+		version:         *chosenVersion,
+	}
+	return q, nil
 }
