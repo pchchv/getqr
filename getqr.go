dda956f9d42e7a80d4aa46684e29be10f58c6de1
pchchv <ipchchv@gmail.com>
2022-08-26 10:01:48 +0300

    Add fields to QR code type

--- getqr.go
+++ getqr.go
@@ -7,11 +7,14 @@ import (
 )
 
 type QRCode struct {
-	Content         string      // Original content encoded
+	Content         string        // Original content encoded
+	Level           RecoveryLevel // QR Code type.
+	VersionNumber   int
 	BackgroundColor color.Color // User settable drawing options
 	ForegroundColor color.Color
 	Border          bool // QR Code border. True — borders are enabled
 	encoder         *dataEncoder
+	version         qrCodeVersion
 	data            *bitset.Bitset
 	synbol          *symbol
 	mask            int
