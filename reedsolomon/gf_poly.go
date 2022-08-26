77b419e9843ede1ec466a3d752a56ffe8cc1caee
pchchv <ipchchv@gmail.com>
2022-08-26 10:39:07 +0300

    Implement a newGFPolyFromData method

--- reedsolomon/gf_poly.go
+++ reedsolomon/gf_poly.go
@@ -1,8 +1,28 @@
 package reedsolomon
 
+import bitset "github.com/pchchv/getqr/bitset"
+
 // Polynomial over GF(2^8).
 type gfPoly struct {
 	// The ith value is the coefficient of the ith degree of x.
 	// term[0]*(x^0) + term[1]*(x^1) + term[2]*(x^2) ...
 	term []gfElement
+}
+
+// Returns data as a polynomial over GF(2^8)
+// Each data byte becomes the coefficient of an x term
+// For an n byte input the polynomial is:
+// data[n-1]*(x^n-1) + data[n-2]*(x^n-2) ... + data[0]*(x^0).
+func newGFPolyFromData(data *bitset.Bitset) gfPoly {
+	numTotalBytes := data.Len() / 8
+	if data.Len()%8 != 0 {
+		numTotalBytes++
+	}
+	result := gfPoly{term: make([]gfElement, numTotalBytes)}
+	i := numTotalBytes - 1
+	for j := 0; j < data.Len(); j += 8 {
+		result.term[i] = gfElement(data.ByteAt(j))
+		i--
+	}
+	return result
 }
