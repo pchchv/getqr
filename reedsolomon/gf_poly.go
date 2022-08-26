5b22db625da6dea208f2459928ab9ac839d73495
pchchv <ipchchv@gmail.com>
2022-08-26 10:44:45 +0300

    Implement a normalised method

--- reedsolomon/gf_poly.go
+++ reedsolomon/gf_poly.go
@@ -31,3 +31,20 @@ func newGFPolyFromData(data *bitset.Bitset) gfPoly {
 func (e gfPoly) numTerms() int {
 	return len(e.term)
 }
+
+func (e gfPoly) normalised() gfPoly {
+	numTerms := e.numTerms()
+	maxNonzeroTerm := numTerms - 1
+	for i := numTerms - 1; i >= 0; i-- {
+		if e.term[i] != 0 {
+			break
+		}
+		maxNonzeroTerm = i - 1
+	}
+	if maxNonzeroTerm < 0 {
+		return gfPoly{}
+	} else if maxNonzeroTerm < numTerms-1 {
+		e.term = e.term[0 : maxNonzeroTerm+1]
+	}
+	return e
+}
