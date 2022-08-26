3d92096bec13f76a615fb123207a6e38dd60be01
pchchv <ipchchv@gmail.com>
2022-08-26 10:45:37 +0300

    Implement a gfPolyAdd method

--- reedsolomon/gf_poly.go
+++ reedsolomon/gf_poly.go
@@ -32,6 +32,28 @@ func (e gfPoly) numTerms() int {
 	return len(e.term)
 }
 
+// Returns a + b.
+func gfPolyAdd(a, b gfPoly) gfPoly {
+	numATerms := a.numTerms()
+	numBTerms := b.numTerms()
+	numTerms := numATerms
+	if numBTerms > numTerms {
+		numTerms = numBTerms
+	}
+	result := gfPoly{term: make([]gfElement, numTerms)}
+	for i := 0; i < numTerms; i++ {
+		switch {
+		case numATerms > i && numBTerms > i:
+			result.term[i] = gfAdd(a.term[i], b.term[i])
+		case numATerms > i:
+			result.term[i] = a.term[i]
+		default:
+			result.term[i] = b.term[i]
+		}
+	}
+	return result.normalised()
+}
+
 func (e gfPoly) normalised() gfPoly {
 	numTerms := e.numTerms()
 	maxNonzeroTerm := numTerms - 1
