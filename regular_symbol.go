3e4d0516a10bcc70455f4d26e8a50bf726c8302d
pchchv <ipchchv@gmail.com>
2022-08-27 11:14:47 +0300

    Implement a addFinderPatterns method

--- regular_symbol.go
+++ regular_symbol.go
@@ -9,3 +9,60 @@ type regularSymbol struct {
 	symbol  *symbol
 	size    int
 }
+
+// Abbreviated true/false.
+const (
+	b0 = false
+	b1 = true
+)
+
+var (
+	finderPattern = [][]bool{
+		{b1, b1, b1, b1, b1, b1, b1},
+		{b1, b0, b0, b0, b0, b0, b1},
+		{b1, b0, b1, b1, b1, b0, b1},
+		{b1, b0, b1, b1, b1, b0, b1},
+		{b1, b0, b1, b1, b1, b0, b1},
+		{b1, b0, b0, b0, b0, b0, b1},
+		{b1, b1, b1, b1, b1, b1, b1},
+	}
+
+	finderPatternSize = 7
+
+	finderPatternHorizontalBorder = [][]bool{
+		{b0, b0, b0, b0, b0, b0, b0, b0},
+	}
+
+	finderPatternVerticalBorder = [][]bool{
+		{b0},
+		{b0},
+		{b0},
+		{b0},
+		{b0},
+		{b0},
+		{b0},
+		{b0},
+	}
+)
+
+func (m *regularSymbol) addFinderPatterns() {
+	fpSize := finderPatternSize
+	fp := finderPattern
+	fpHBorder := finderPatternHorizontalBorder
+	fpVBorder := finderPatternVerticalBorder
+
+	// Top left Finder Pattern.
+	m.symbol.set2dPattern(0, 0, fp)
+	m.symbol.set2dPattern(0, fpSize, fpHBorder)
+	m.symbol.set2dPattern(fpSize, 0, fpVBorder)
+
+	// Top right Finder Pattern.
+	m.symbol.set2dPattern(m.size-fpSize, 0, fp)
+	m.symbol.set2dPattern(m.size-fpSize-1, fpSize, fpHBorder)
+	m.symbol.set2dPattern(m.size-fpSize-1, 0, fpVBorder)
+
+	// Bottom left Finder Pattern.
+	m.symbol.set2dPattern(0, m.size-fpSize, fp)
+	m.symbol.set2dPattern(0, m.size-fpSize-1, fpHBorder)
+	m.symbol.set2dPattern(fpSize, m.size-fpSize-1, fpVBorder)
+}
