f8656a7c823a0e7f14df2cf7b2b4162f5467b656
pchchv <ipchchv@gmail.com>
2022-08-30 12:57:00 +0300

    Implement a equal func

---
+++ bitset/bitset_test.go
@@ -0,0 +1,13 @@
+package bitset
+
+func equal(a []bool, b []bool) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
